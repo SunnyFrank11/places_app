bc8330135635e9339e747708ca0ce9dc3196b0bb
SunnyFrank11 <chukwuekesunny12@gmail.com>
2022-12-04 20:35:44 +0100

    map endpoint added

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,8 @@
+const apiKey = 'AIzaSyAiY9cmxxn9V51u0r_bVg5wy8HpkHJRb6Y';
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {required double latitude, required double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x500&maptype=hybrid&markers=color:red%7Clabel:S%7C$latitude,$longitude&key=$apiKey';
+  }
+}
